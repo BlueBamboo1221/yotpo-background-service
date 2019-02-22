3a28ca5c34f5f07ede0aec85d7904ad643a690e5
DESKTOP-GSFNU4J\RKI <bluebamboo1221@outlook.com>
2019-02-22 19:21:30 +0200

    algolia app key

--- algolia/algolia.js
+++ algolia/algolia.js
@@ -1,7 +1,7 @@
 const algoliasearch = require('algoliasearch')
 
-ALGOLIA_APP_ID="CUDCYZHOXL"
-ALGOLIA_API_KEY="0688032a3dfc48a81a666ae598b66c90"
+ALGOLIA_APP_ID="***"
+ALGOLIA_API_KEY="***"
 
 const algolia = algoliasearch(
   ALGOLIA_APP_ID,
